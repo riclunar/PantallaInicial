996a9b5c9018e95c322910ec6bb6304216f475db
riclunar <63484908+riclunar@users.noreply.github.com>
2020-04-25 02:28:33 -0500

    Pantalla de bienvenida Ejercicio20203

---
+++ app/src/main/java/mx/edu/tesoem/isc/riclr/pantallainicial/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.riclr.pantallainicial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
